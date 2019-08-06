b75d94830643ac940d17aabe0a2f2065a086cae0
Bogdan Floris <bogdan.floris@gmail.com>
2019-08-06 21:59:42 +0200

    Added console as a library inside this repo

---
+++ console/src/kb.rs
@@ -0,0 +1,18 @@
+/// Key mapping
+///
+/// This is an incomplete mapping of keys that are supported for reading
+/// from the keyboard.
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum Key {
+    Unknown,
+    ArrowLeft,
+    ArrowRight,
+    ArrowUp,
+    ArrowDown,
+    Enter,
+    Escape,
+    Backspace,
+    Char(char),
+    #[doc(hidden)]
+    __More,
+}
